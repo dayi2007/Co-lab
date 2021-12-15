95c8942baee5171b54a6d6188bdb2a315074e06a
Dayi <dayi2007@hotmail.com>
2021-12-14 19:54:19 -0500

    screens

--- client/src/screens/Profile.jsx
+++ client/src/screens/Profile.jsx
@@ -4,10 +4,11 @@ import './Profile.css'
 import { useParams } from "react-router";
 import edit from '../assets/edit.png';
 import trash from '../assets/binPaper.png';
-import addNew from '../assets/messagesAdd.png';
+import create from '../assets/messagesAdd.png';
 import logout from '../assets/logout.png';
 import update from '../assets/update.png';
 import like from '../assets/favorite.png';
+import add from '../assets/star.png';
 
 export default function Profile({currentUser, posts, handlePostDelete, handleLogout }){
     console.log(currentUser.id)
@@ -32,70 +33,85 @@ export default function Profile({currentUser, posts, handlePostDelete, handleLog
 
 return(
 <div>
-    {currentUser.id === parseInt(id) ? (
+    {
+    currentUser.id === parseInt(id) ? (
     <div>    
-            <img className="profileImg" src={currentUser.picture} alt="userPicture"/> 
-            <Link className="logoOverlap" to='/'><img src={logo} alt="Co-Lab"/></Link>
+        <img className="profileImg" src={currentUser.picture} alt="userPicture"/> 
+        <Link className="logoOverlap" to='/'><img src={logo} alt="Co-Lab"/></Link>
         <div className="userInfo">
-                <h1>{currentUser.username}</h1> 
-                <Link to ='/create'><button><img src={addNew} alt="create"/></button></Link> 
-                <Link to={`/users/${newId}/edit`}> <button><img src={update} alt="update"/></button></Link>
+            <h1 className="nameUser">{currentUser.username}</h1> 
+            <div className="profileButtons">
+                <div>
+                    <Link to ='/create'><button><img src={create} alt="create"/></button></Link> 
+                    <Link to={`/users/${newId}/edit`}> <button><img src={update} alt="update"/></button></Link>
+                </div>
                 <div>
                     <button onClick={handleLogout}><img src={logout} alt="logout"/></button>
                 </div>
-            <div className='postsOutter'>
-        
-            {
-            posts.map((post) =>(
-                <div key={post.id} className='card'>
-                {currentUser?.id === post.user_id && (
-                    <div>
-                    <p>{post.message}</p>
-                    <img className="imgPost" src={post.picture} alt="userPicture"/>
-                    <h3>{post.user.username}</h3>                  
+            </div>
+            <div className='postsOutter'>        
+                {
+                userPosts.map((post) =>(
+                    currentUser?.id === post.user_id && (
+                    <div key={post.id} className='card'>
+                        <div className="profileLink">
+                        <div className="linkProfile">
+                            <img  className="imgPostUser" src={post.user.picture} alt="UserImage"/>
+                            <h7>{post.user.username}</h7>
+                        </div>
+                        </div>
+                        <div className="messagePicture">
+                            <p className="message">{post.message}</p>
+                            <img className="imgPost" src={post.picture} alt="Post Img"/>  
+                        </div>  
                         <div className="editDelete"> 
                             <Link to={`/posts/${post.id}/edit`}>
                                 <button><img src={edit}/></button>
                             </Link>                       
                             <button onClick={() => handlePostDelete(post.id)}><img src={trash}/></button>
                         </div> 
-                    </div>)}              
-                </div>
-            ))
-            }
+                    </div>
+                    )
+                ))
+                }
             </div>
         </div> 
     </div>  
-        ) : (
-            <div> 
-                <img className="profileImg" src={userPicture} alt="userPicture"/>
-                <Link className="logoOverlap" to='/'><img src={logo} alt="Co-Lab"/></Link>
-                <div className="userInfo">
-                    <h1 className="nameUser">{userUsername}</h1>
-                </div>
-                <div className='postsOutterUser'>                
-                    {userPosts.map((post) =>( 
-                        <div key={post.id}className="card">
-                            <div className="profileLink">
-                                <div className="linkProfile">
-                                    <img  className="imgPostUser" src={post.user.picture} alt="UserImage"/>
-                                <h7>{post.user.username}</h7>
-                                </div>
+    ) : (
+    <div> 
+        <img className="profileImg" src={userPicture} alt="userPicture"/>
+        <Link className="logoOverlap" to='/'><img src={logo} alt="Co-Lab"/></Link>
+        <div className="userInfo">
+            <div className="headerUser">
+                <h1 className="nameUser">{userUsername}</h1>
+                <button className="addUser"><img src={add} alt="add"/> </button>
+            </div>        
+        <div className='postsOutterUser'>                
+            {
+            userPosts.map((post) =>( 
+                <div key={post.id}className="card">
+                    <div className="profileLink">
+                        <div className="linkProfile">
+                            <img  className="imgPostUser" src={post.user.picture} alt="UserImage"/>
+                            <h7>{post.user.username}</h7>
+                        </div>
+                    </div>
+                    <div className="messagePicture">
+                        <p className="message">{post.message}</p>
+                        <img className="imgPost" src={post.picture} alt="Post Img"/>  
+                            <div className="iconsCard"> 
+                                <button><img src={like} alt="like"/></button>  
                             </div>
-                            <div className="messagePicture">
-                                <p className="message">{post.message}</p>
-                                <img className="imgPost" src={post.picture} alt="Post Img"/>  
-                                    <div className="iconsCard"> 
-                                    <button><img src={like} alt="like"/></button>  
-                                    </div>
-                            </div>  
-                        </div> 
-                    ))
-                    }
-                </div>
-            </div> )
+                    </div>  
+                </div> 
+            ))
+            }
+        </div>
+        </div>
+    </div> 
+    )
 
-        }
+    }
 </div>
 );
 }
